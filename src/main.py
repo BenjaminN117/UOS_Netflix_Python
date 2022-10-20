667efb12ac48149bc0c52530b29ec44ef5a857a5
seasickcake <59026128+seasickcake@users.noreply.github.com>
2022-10-20 13:09:44 +0100

    move function to a new file
    I moved the function out of the main.py file into it own file of country_search.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,18 @@
 '''
-Product:Country search feature
-Description: will display the number of shows that are from the searched country.
-Author: Seasickcake
+Product:
+Description:
+Author:
 '''
-import csv
+from databaseImporter import csvImport
+import pandas as pd
+import matplotlib
 
-print ("This program will display the number of netflix titles from X country.")
-answer = input("Name of the county you wish to search?")
-
-count  = 0
-with open('data\\netflix_titles.csv' , encoding="utf8") as csv_file:
-    spamreader = csv.DictReader(csv_file)
-    for i,row in enumerate(spamreader):
-        if (row['country']==answer) :
-            count = count + 1
-        if(i >= 8806):
-            print ("There are",count, "netflix titles that are from the",answer)
-            break
+class Solution():
+    def __init__(self):
+        inst = csvImport()
+        df = inst.import_database()
+            
+        
+if __name__ == "__main__":
+    obj = Solution()
+    
